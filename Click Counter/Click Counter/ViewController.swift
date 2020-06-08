eeeb76ef75ad3e41e8e2c7385dd6fa8d017b8df3
Yini <ydxb7@mail.missouri.edu>
2020-06-07 20:08:40 -0700

    addTarget to button

--- Click Counter/Click Counter/ViewController.swift
+++ Click Counter/Click Counter/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var count = 0
+    var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +21,7 @@ class ViewController: UIViewController {
         label.frame = CGRect(x: 150, y: 150, width: 60, height: 60)
         label.text = "0"
         view.addSubview(label)
+        self.label = label
         
         // button
         let button = UIButton()
@@ -25,8 +29,14 @@ class ViewController: UIViewController {
         button.setTitle("Click", for: .normal)
         button.setTitleColor(UIColor.blue, for: .normal)
         view.addSubview(button)
+        
+        button.addTarget(self, action: #selector(ViewController.inccrementCount), for: UIControl.Event.touchUpInside)
     }
 
+    @objc func inccrementCount() {
+        self.count += 1
+        self.label.text = "\(self.count)"
+    }
 
 }
 
