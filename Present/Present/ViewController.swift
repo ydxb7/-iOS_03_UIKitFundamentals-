6f6323bfe9ae24c4e6b2b6a088f95bb1abc3e286
Yini <ydxb7@mail.missouri.edu>
2020-06-13 16:38:30 -0700

    Present - present activity and alert controller

--- Present/Present/ViewController.swift
+++ Present/Present/ViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet var presentButton: UIButton!
+    @IBOutlet var activityButton: UIButton!
+    @IBOutlet var alertButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,26 @@ class ViewController: UIViewController {
     @IBAction func experiment(sender: UIButton) {
         let nextController = UIImagePickerController()
         present(nextController, animated: true, completion: nil)
+    }
+    
+    @IBAction func activity(sender: UIButton) {
+        let image = UIImage()
+        let controller = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        present(controller, animated: true, completion: nil)
+    }
+    
+    @IBAction func alert(sender: UIButton) {
+        let controller = UIAlertController()
+        
+        controller.title = "Test Alert"
+        controller.message = "This is a test"
+        
+        let okAction = UIAlertAction(title: "ok", style: UIAlertAction.Style.default) { action in
+            self.dismiss(animated: true, completion: nil)
+        }
+        
+        controller.addAction(okAction)
+        present(controller, animated: true, completion: nil)
     }
     
 }
